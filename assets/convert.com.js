5d51bed3a67da4a11aa943aca1156b87075ef49f
Murrell Johnson <dev@klikz.us>
2023-06-29 16:28:31 -0400

    chore: optimize to convert.com migration

---
+++ assets/convert.com.js
@@ -0,0 +1,21 @@
+function activateAddToCartGoal() {
+  window._conv_q = window._conv_q || [];
+  _conv_q.push(['triggerConversion', '10046163']);
+}
+
+const targetEl = document.getElementById('cart-drawer');
+
+// create a new observer instance
+const cartObserver = new MutationObserver(function (mutations) {
+  mutations.forEach(function (mutation) {
+    if (
+      mutation.attributeName === 'class' &&
+      mutation.target.classList.contains('opened-drawer')
+    ) {
+      activateAddToCartGoal();
+    }
+  });
+});
+
+// pass in the target node, as well as the observer options
+cartObserver.observe(targetEl, { attributes: true });

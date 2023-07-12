17358f4b8622463a576147df21684a52b0b0af18
Lee Daniel <42626218+leedaniel2018@users.noreply.github.com>
2023-07-12 10:29:38 -0500

    feat: fix form build

--- assets/return-auth-form-block.js
+++ assets/return-auth-form-block.js
@@ -1,10 +1,10 @@
-const purchaseWay1 = document.getElementById("purchaseDirectly");
+const purchaseWay1 = document.getElementById("ContactForm-purchase-method-direct");
 purchaseWay1.addEventListener('change', function() {
     document.getElementById("orderNumberInput").classList.remove("d-none");
     document.getElementById("businessNameInput").classList.add("d-none");
 });
 
-const purchaseWay2 = document.getElementById("purchaseDirectlyNo");
+const purchaseWay2 = document.getElementById("ContactForm-purchase-method-nodirect");
 purchaseWay2.addEventListener('change', function() {
     document.getElementById("orderNumberInput").classList.add("d-none");
     document.getElementById("businessNameInput").classList.remove("d-none");

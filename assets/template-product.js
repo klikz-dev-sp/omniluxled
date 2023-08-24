298dd828c81af6c3280137afc0bf739c307a42b5
Murrell Johnson <dev@klikz.us>
2023-08-24 10:14:04 -0400

    chore: faq on pdp bug

--- assets/template-product.js
+++ assets/template-product.js
@@ -59,22 +59,20 @@ class model3d {
 
 typeof model3d !== 'undefined' && new model3d();
 
-window.onload = function(){
-  if ($("body").hasClass("ab-faq template-product omnilux-contour-face")) {   
+$(document).ready(function () {
     $("#gorgias-chat-start").click(function(){   
-      if($('#gorgias-chat-container #chat-window').css('display') == 'none'){      
-        $("#gorgias-chat-container #chat-button").contents().find('button[data-testid="gorgias-chat-messenger-button"]').click();  
-      }
-      
-      if($('#gorgias-chat-container #chat-button').css('visibility') == 'hidden'){      
-        $('#gorgias-chat-container #chat-button').attr('style', 'right: 20px;bottom: 20px;visibility: visible !important;width: 72px;height: 66px;');
-      }  
-    }) 
+    if($('#gorgias-chat-container #chat-window').css('display') == 'none'){      
+      $("#gorgias-chat-container #chat-button").contents().find('button[data-testid="gorgias-chat-messenger-button"]').click();  
+    }
     
-    $("#gorgias-chat-container #chat-button").contents().find('button[data-testid="gorgias-chat-messenger-button"]').click(function(){    
-      if($('#gorgias-chat-container #chat-button').css('visibility') == 'visible'){      
-        $('#gorgias-chat-container #chat-button').attr('style', 'visibility: hidden !important');
-      }  
-    })  
-  }
-};
+    if($('#gorgias-chat-container #chat-button').css('visibility') == 'hidden'){      
+      $('#gorgias-chat-container #chat-button').attr('style', 'right: 20px;bottom: 20px;visibility: visible !important;width: 72px;height: 66px;');
+    }  
+  }) 
+  
+  $("#gorgias-chat-container #chat-button").contents().find('button[data-testid="gorgias-chat-messenger-button"]').click(function(){    
+    if($('#gorgias-chat-container #chat-button').css('visibility') == 'visible'){      
+      $('#gorgias-chat-container #chat-button').attr('style', 'visibility: hidden !important');
+    }  
+  })  
+});

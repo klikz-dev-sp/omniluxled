cbfae4e8c03754c2271fa8f73f36bc14fbf5f25c
Murrell Johnson <dev@klikz.us>
2023-08-25 15:48:49 -0400

    chore: faq experiment

--- assets/convert.com.js
+++ assets/convert.com.js
@@ -23,24 +23,23 @@ const cartObserver = new MutationObserver(function (mutations) {
 // pass in the target node, as well as the observer options
 cartObserver.observe(targetEl, { attributes: true });
 
-
 /**
  * FAQ on PDP - Above the fold
  */
-const faqObserver = new IntersectionObserver(function (entries) {
-  entries.forEach(function (entry) {
-    if (entry.isIntersecting) {
-      if (window.runExperiment != 2) {
-        window.runExperiment = 2;
 
-        window._conv_q = window._conv_q || [];
-        window._conv_q.push(['executeExperiment', '100412969']);
-        // trigger "faq reached" goal
-        window._conv_q.push(["triggerConversion", "100411732"]);
-        console.log('Activated FAQs A/B Experiment');
-      }
+window.addEventListener('scroll', function (e) {
+  const fold = document.getElementById('section-faq').parentElement.offsetTop;
+
+  if (this.scrollY > fold) {
+    if (this.window.runExperiment != 2) {
+      window.runExperiment = 2;
+
+      window._conv_q = window._conv_q || [];
+      window._conv_q.push(['executeExperiment', '100412969']);
+
+      window._conv_q.push(['triggerConversion', '100411732']);
+
+      console.log('Activated FAQs A/B Experiment');
     }
-  });
+  }
 });
-
-faqObserver.observe(document.getElementById('section-faq'));

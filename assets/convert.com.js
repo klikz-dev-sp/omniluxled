6c5025bb2fece1b7cce0f6b7dc14ef2efa770b89
Murrell Johnson <dev@klikz.us>
2023-08-25 16:26:34 -0400

    chore: convert

--- assets/convert.com.js
+++ assets/convert.com.js
@@ -1,45 +1,20 @@
-function activateAddToCartGoal() {
-  window.runExperiment = 1;
-
-  window._conv_q = window._conv_q || [];
-  window._conv_q.push(['triggerConversion', '10046163']);
-  _conv_q.push(['triggerConversion', '10046163']);
-}
-
-const targetEl = document.getElementById('cart-drawer');
-
-// create a new observer instance
-const cartObserver = new MutationObserver(function (mutations) {
-  mutations.forEach(function (mutation) {
-    if (
-      mutation.attributeName === 'class' &&
-      mutation.target.classList.contains('opened-drawer')
-    ) {
-      activateAddToCartGoal();
-    }
-  });
-});
-
-// pass in the target node, as well as the observer options
-cartObserver.observe(targetEl, { attributes: true });
-
 /**
  * FAQ on PDP - Above the fold
  */
+let fold =
+  document.getElementById('section-faq')?.parentElement?.offsetTop || 500;
 
 window.addEventListener('scroll', function (e) {
-  const fold = document.getElementById('section-faq').parentElement.offsetTop;
-
-  if (this.scrollY > fold) {
-    if (this.window.runExperiment != 2) {
-      window.runExperiment = 2;
+  if (this.scrollY > fold - window.innerHeight) {
+    if (window.runExperiment != 1) {
+      window.runExperiment = 1;
 
       window._conv_q = window._conv_q || [];
       window._conv_q.push(['executeExperiment', '100412969']);
 
-      window._conv_q.push(['triggerConversion', '100411732']);
-
       console.log('Activated FAQs A/B Experiment');
+
+      _conv_q.push(['triggerConversion', '100411732']);
     }
   }
 });
